f65271bb4dca9ae72d7cd0b983ccdffd16dc6849
Manik Sahai <manik-sahai@users.noreply.github.com>
2017-07-01 05:46:49 +0530

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,34 +1,45 @@
 import media  # contains the movie class which has characteristics of a movie
-import fresh_tomatoes  # contains the layout of the web page and the function to render the page
+import fresh_tomatoes  # contains the layout of the web page and the
+
+# function to render the page
 
 # instances of class movie to populate the web page with actual movie data
 
-thor_ragnarok = media.Movie("Thor: Ragnarock", "http://empireonline.media/jpg/70/0/0/1280/960/aspectfit/0/0/0/0/0/0/c/articles/58c043053302c94d18f73263/thor-ragnarok-ew-cover.jpg",  # NOQA
-                       "https://www.youtube.com/watch?v=v7MGUNV8MxU")
+thor_ragnarok = media.Movie("Thor: Ragnarock", "https://goo.gl/9vyyef",
+                            "https://www.youtube.com/watch?v=v7MGUNV8MxU")
 
-spider_man = media.Movie("Spiderman:Homecoming", "https://i.imgbox.com/574OovOo.jpg", "https://www.youtube.com/watch?v=xEvV3OsE2WM")  # NOQA
+spider_man = media.Movie("Spiderman:Homecoming",
+                         "https://i.imgbox.com/574OovOo.jpg",
+                         "https://www.youtube.com/watch?v=xEvV3OsE2WM")
 
-justice_league = media.Movie("Justice League", "http://orig10.deviantart.net/1e5f/f/2017/061/1/a/justice_league_movie_poster_4_by_jackjack671120-db10aub.jpg",  # NOQA
-                            "https://www.youtube.com/watch?v=3cxixDgHUYw")
+justice_league = media.Movie("Justice League", "https://goo.gl/A5LbyF",
+                             "https://www.youtube.com/watch?v=3cxixDgHUYw")
 
-wonder_woman = media.Movie("Wonder Woman", "http://cdn2-www.superherohype.com/assets/uploads/gallery/wonder-woman/wwpost345.jpg",  # NOQA
-                          "https://www.youtube.com/watch?v=VSB4wGIdDwo")
+wonder_woman = media.Movie("Wonder Woman", "https://goo.gl/3E9Fam",
+                           "https://www.youtube.com/watch?v=VSB4wGIdDwo")
 
-iron_man = media.Movie("Iron Man", "http://www.impawards.com/2008/posters/iron_man_ver3_xlg.jpg", "https://www.youtube.com/watch?v=8hYlB38asDY")  # NOQA
+iron_man = media.Movie("Iron Man", "https://goo.gl/eB4FFV",
+                       "https://www.youtube.com/watch?v=8hYlB38asDY")
 
-the_avengers = media.Movie("The Avengers", "http://orig15.deviantart.net/f2e7/f/2010/228/c/3/__the_avengers___movie_poster_by_themadbutcher.jpg",  # NOQA
-                          "https://www.youtube.com/watch?v=eOrNdBpGMv8")
+the_avengers = media.Movie("The Avengers", "https://goo.gl/D637P1",
+                           "https://www.youtube.com/watch?v=eOrNdBpGMv8")
 
-guardians_of_the_galaxy = media.Movie("Guardians of the Galaxy", "http://agentpalmer.com/wp-content/uploads/2014/08/Official-Guardians-of-the-Galaxy-Movie-Poster.jpg",  # NOQA
-                                   "https://www.youtube.com/watch?v=d96cjJhvlMA")
+guardians_of_the_galaxy = media.Movie("Guardians of the Galaxy",
+                                      "https://goo.gl/UjjwUn",
+                                      "https://www.youtube.com/watch?v=d96cjJhvlMA")  # NOQA
 
-jurassic_park = media.Movie("Jurassic Park", "http://moviereviews.s3.amazonaws.com/2016/04/11/17/47/51/994/oW484gwIHBk7haWwIunUuySqPfx.jpg",  # NOQA
-                           "https://www.youtube.com/watch?v=_IesovZQR4g")
+jurassic_park = media.Movie("Jurassic Park", "https://goo.gl/wK6Gio",
+                            "https://www.youtube.com/watch?v=_IesovZQR4g")
 
-the_greatest_game_ever_played = media.Movie("The Greatest Game Ever Played", "https://i.jeded.com/i/the-greatest-game-ever-played.18712.jpg",  # NOQA
-                                        "https://www.youtube.com/watch?v=8xK9yXKldrk")
+the_greatest_game_ever_played = media.Movie("The Greatest Game Ever Played",
+                                            "https://goo.gl/zWqjr2",
+                                            "https://www.youtube.com/watch?v=8xK9yXKldrk")  # NOQA
 
-movies = [thor_ragnarok, spider_man, justice_league, wonder_woman, iron_man, the_avengers, guardians_of_the_galaxy,
-          jurassic_park, the_greatest_game_ever_played]  # list containing movies
 
-fresh_tomatoes.open_movies_page(movies)  # function to render web page containing data from a list of movies
+# list containing movies
+movies = [thor_ragnarok, spider_man, justice_league, wonder_woman, iron_man,
+          the_avengers, guardians_of_the_galaxy, jurassic_park,
+          the_greatest_game_ever_played]
+
+# function to render web page containing data from a list of movies
+fresh_tomatoes.open_movies_page(movies)

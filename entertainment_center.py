32d2606d7ec3bbead242ff9ea56292b38651bd47
Manik Sahai <manik-sahai@users.noreply.github.com>
2017-07-01 05:57:07 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,7 +3,6 @@ import fresh_tomatoes  # contains the layout of the web page and the
 # function to render the page
 
 # instances of class movie to populate the web page with actual movie data
-
 thor_ragnarok = media.Movie("Thor: Ragnarock", "https://goo.gl/9vyyef",
                             "https://www.youtube.com/watch?v=v7MGUNV8MxU")
 
